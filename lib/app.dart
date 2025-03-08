4cddd92627da61a44eccf1bd0fe02f0ae51c17fb
Code On Saturday <devfloody@gmail.com>
2025-03-08 18:26:19 +0700

    feat: implement routing and add product page

--- lib/app.dart
+++ lib/app.dart
@@ -1,36 +1,47 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:kasirtoko/src/core/core.dart';
 
 import 'flavors.dart';
-import 'pages/my_home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: F.title,
-      theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: Map<TargetPlatform, PageTransitionsBuilder>.fromIterable(
+            TargetPlatform.values,
+            value: (_) => const FadeForwardsPageTransitionsBuilder(),
+          ),
+        ),
+      ),
       builder: (context, child) {
         ErrorWidget.builder = (errorDetails) {
-          return Container();
+          return Container(
+            color: Theme.of(context).canvasColor,
+            child: Center(child: Text(errorDetails.exception.toString())),
+          );
         };
         return child!;
       },
-      home: _flavorBanner(child: MyHomePage(), show: kDebugMode),
+      routerConfig: goRouterConfig,
     );
   }
 
-  Widget _flavorBanner({required Widget child, bool show = true}) =>
-      show
-          ? Banner(
-            location: BannerLocation.topStart,
-            message: F.name,
-            color: Colors.green,
-            textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 12.0, letterSpacing: 1.0),
-            textDirection: TextDirection.ltr,
-            child: child,
-          )
-          : Container(child: child);
+  // Widget _flavorBanner({required Widget child, bool show = true}) =>
+  //     show
+  //         ? Banner(
+  //           location: BannerLocation.topStart,
+  //           message: F.name,
+  //           color: Colors.green,
+  //           textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 12.0, letterSpacing: 1.0),
+  //           textDirection: TextDirection.ltr,
+  //           child: child,
+  //         )
+  //         : Container(child: child);
 }
