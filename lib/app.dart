561f3506448f462fb1547c81c81499a384bdd933
fahmi <devfloody@gmail.com>
2025-03-10 10:33:55 +0700

    re enable ios

--- lib/app.dart
+++ lib/app.dart
@@ -4,37 +4,35 @@ import 'package:kasirtoko/src/core/core.dart';
 import 'flavors.dart';
 
 class App extends StatelessWidget {
-
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: F.title,
-      debugShowCheckedModeBanner: false,
       theme: COSThemes.lightTheme,
-      builder: (context, child) {
-        ErrorWidget.builder = (errorDetails) {
-          return Container(
-            color: Theme.of(context).canvasColor,
-            child: Center(child: Text(errorDetails.exception.toString())),
-          );
-        };
-        return child!;
-      },
       routerConfig: goRouterConfig,
+      debugShowCheckedModeBanner: false,
     );
   }
 
-  // Widget _flavorBanner({required Widget child, bool show = true}) =>
+  // Widget _flavorBanner({
+  //   required Widget child,
+  //   bool show = true,
+  // }) =>
   //     show
   //         ? Banner(
-  //           location: BannerLocation.topStart,
-  //           message: F.name,
-  //           color: Colors.green,
-  //           textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 12.0, letterSpacing: 1.0),
-  //           textDirection: TextDirection.ltr,
-  //           child: child,
-  //         )
-  //         : Container(child: child);
+  //       child: child,
+  //       location: BannerLocation.topStart,
+  //       message: F.name,
+  //       color: Colors.green.withOpacity(0.6),
+  //       textStyle: TextStyle(
+  //           fontWeight: FontWeight.w700,
+  //           fontSize: 12.0,
+  //           letterSpacing: 1.0),
+  //       textDirection: TextDirection.ltr,
+  //     )
+  //         : Container(
+  //       child: child,
+  //     );
 }
