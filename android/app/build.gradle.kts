7637370947491e5e7a67518f5786907e09760464
fahmi <devfloody@gmail.com>
2025-03-17 15:08:18 +0700

    fix: update ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
 
     namespace = "app.strdy.kasirtoko"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
