e8fe252f42a857208ddc6cbe29263e38e6a32bee
Code On Saturday <devfloody@gmail.com>
2025-03-22 14:59:26 +0700

    refactor: Remove unused flavorBanner widget and update SearchBar hint text

--- lib/app.dart
+++ lib/app.dart
@@ -15,24 +15,4 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
     );
   }
-
-  // Widget _flavorBanner({
-  //   required Widget child,
-  //   bool show = true,
-  // }) =>
-  //     show
-  //         ? Banner(
-  //       child: child,
-  //       location: BannerLocation.topStart,
-  //       message: F.name,
-  //       color: Colors.green.withOpacity(0.6),
-  //       textStyle: TextStyle(
-  //           fontWeight: FontWeight.w700,
-  //           fontSize: 12.0,
-  //           letterSpacing: 1.0),
-  //       textDirection: TextDirection.ltr,
-  //     )
-  //         : Container(
-  //       child: child,
-  //     );
 }
